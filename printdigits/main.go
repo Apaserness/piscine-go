ca5a8818198226eb87c0d319d7857cab698bbd40
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-16 20:40:00 +0600

    Update main.go

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	i = 0
-	for i <= 9 {
-		z01.PrintRune(i)
-		i++
+	var d = 0
+	for d <= 9 {
+		z01.PrintRune(d)
+		d++
 	}
 	z01.PrintRune('\n')
 }
