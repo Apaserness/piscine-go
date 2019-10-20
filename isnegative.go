ea4a6a97ddcefe95844a66c4ee2d976d82014f79
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-20 10:24:03 +0600

    Update isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,12 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb < 0 {
-		z01.PrintRune('T')
-	} else {
+	if nb >= 0 {
 		z01.PrintRune('F')
+	} else {
+		z01.PrintRune('T')
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
