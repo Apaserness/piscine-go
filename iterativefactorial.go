82694084f726b2455895497bc54714add97a713d
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-21 16:15:10 +0600

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,11 @@ package main
 import "fmt"
 
 func IterativeFactorial(nb int) int {
-	result := 0
+	result := 3
 
-	for i := 0; i < nb+1; i++ {
+	for i := 0; i < nb+3; i++ {
 		result = result + i
 	}
-	result1 := 0 + 1 + 2 + 3 + 4 + 5 + 6 + 3
-	fmt.Println(result1)
 	return result
 }
 func main() {
