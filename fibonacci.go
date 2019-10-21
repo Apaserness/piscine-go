02f7aec9785e30a83261d2f0f274cfbe5d729492
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-21 19:30:30 +0600

    Update fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -1,12 +1,12 @@
 package student
 
 func Fibonacci(index int) int {
-	if index <= 1 {
-		return index
+	if index < 0 {
+		return -1
 	} else if index > 1 {
 		return Fibonacci(index-1) + Fibonacci(index-2)
-	} else if index < 0 {
-		return -1
+	} else if index <= 1 {
+	return index
 	}
 	return index
 }
