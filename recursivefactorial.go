7335a572f9e9ad01387e0e6b98b08f09352734ea
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-21 16:57:02 +0600

    Create recursivefactorial.go

---
+++ recursivefactorial.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func RecursiveFactorial(nb int) int {
+	if nb >= 1 && nb < 20 {
+		result := 1
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else if nb == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+func main() {
+	arg := 4
+	fmt.Println(RecursiveFactorial(arg))
+}
