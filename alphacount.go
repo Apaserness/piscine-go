d97365d205e11c34ea6a419773a69321aae0a38c
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-22 15:09:44 +0600

    Update alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,10 @@
 package student
 
-import "fmt"
 
 func AlphaCount(str string) int {
 	counter := 0
 	for index, letter := range str {
 		counter++
-		fmt.Println(index, letter)
 
 	}
 	return counter
