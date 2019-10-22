ac7324305ca82d4f269328f60d614d362324d949
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-22 15:30:44 +0600

    Update alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -2,9 +2,15 @@ package student
 
 func AlphaCount(str string) int {
 	counter := 0
-	for index, letter := range str {
-		counter++
-
+	len := 0
+	for i := range str {
+		len = i
+	}
+	for i := 0; i <= len; i++ {
+		result++
+		if !(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' && str[i] <= 'z') {
+			result--
+		}
 	}
 	return counter
 }
