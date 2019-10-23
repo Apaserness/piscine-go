d276d15b68c8eee34f101e6a0b89f3e6c7333a55
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-23 17:57:58 +0600

    Create islower.go

---
+++ islower.go
@@ -0,0 +1,16 @@
+package student
+
+func IsLower(str string) bool {
+	if str == "" {
+		return false
+	}
+	s := []rune(str)
+	for _, letter := range s {
+		if letter >= 'a' && letter <= 'z' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
