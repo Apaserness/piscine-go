7f429df8099931e0813dae35655c5d239075cd6f
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-23 15:08:02 +0600

    Update nrune.go

--- nrune.go
+++ nrune.go
@@ -2,9 +2,9 @@ package student
 
 func NRune(s string, n int) rune {
 	s1 := []rune(s)
-	for index, char = range s1 {
-		if index == index - 1 {
-		return char
+	for index, char := range s1 {
+		if index == n - 1 {
+			return char
 		}
 	}
 	return 0
