a930b066b7464f2a2463793d68bddeeadfd3a66e
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-23 14:53:57 +0600

    Create trimatoi.go

---
+++ trimatoi.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func TrimAtoi(fraza string) int {
+    runes := []rune(fraza)
+    sign := 1
+    result := 0
+    foundDigit := false
+    for _, r := range runes {
+        if !foundDigit {
+            if r == '-' {
+                sign = -1
+            } else if r == '+' {
+                sign = 1
+            }
+        }
+        if r >= '0' && r <= '9' {
+            foundDigit = true
+            result = 10*result + int(r-'0')
+        }
+    }
+    return sign * result
+}
