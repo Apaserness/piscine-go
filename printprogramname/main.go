79c67bbbd802ffe1e7becc94726ce75d3ab6b3e4
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-23 22:20:09 +0600

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,10 +1,7 @@
 package student
 
-import (
-	"os"
-	"github.com/01-edu/z01"
-)
-
+import "os"
+import "github.com/01-edu/z01"
 func main() {
 	name := []rune(os.Args[0])
 	for _, r := range name {
