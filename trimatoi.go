3e871ce4581077aec49acefeb4c5fe8197fe949d
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-23 14:56:54 +0600

    Update trimatoi.go

--- trimatoi.go
+++ trimatoi.go
@@ -1,22 +1,44 @@
-package student
+package piscine
 
-func TrimAtoi(fraza string) int {
-	runes := []rune(fraza)
-    sign := 1
-    result := 0
-    foundDigit := false
-    for _, r := range runes {
-        if !foundDigit {
-            if r == '-' {
-                sign = -1
-            } else if r == '+' {
-                sign = 1
-            }
-        }
-		if r >= '0' && r <= '9' {
-            foundDigit = true
-            result = 10*result + int(r-'0')
-        }
-    }
-    return sign * result
+func IntCheck(s string) bool {
+	ok := false
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			ok = true
+			break
+		}
+	}
+	return ok
+}
+
+func MinusCheck(s string) bool {
+	minus := false
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			break
+		}
+		if c == '-' {
+			minus = true
+		}
+	}
+	return minus
+}
+
+func TrimAtoi(s string) int {
+	x := 0
+	if IntCheck(s) == true {
+		for _, c := range s {
+			if c >= '0' && c <= '9' {
+				k := 0
+				for i := '1'; i <= c; i++ {
+					k++
+				}
+				x = x*10 + k
+			}
+		}
+	}
+	if MinusCheck(s) {
+		x *= -1
+	}
+	return x
 }
