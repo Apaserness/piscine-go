ac3c830b1946e32cf8495f4c83fb3d2046404ce3
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-24 15:09:10 +0600

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,12 +1,13 @@
-package student
+package main
 
-import ( 
+import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	name := []rune(os.Args[0])
+	name := []rune(os.Args[0]) //convert from string to array
 	for _, r := range name {
 		z01.PrintRune(r)
 	}
